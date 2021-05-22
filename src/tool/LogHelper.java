975cfc84c94cc9e65fa1b4a6fbe1f9b25cf66cde
CuiiGen <1029374457@qq.com>
2021-05-22 11:47:46 +0800

    异常信息格式化工具

---
+++ src/tool/LogHelper.java
@@ -0,0 +1,21 @@
+package tool;
+
+import java.util.ArrayList;
+
+public class LogHelper {
+
+	/**
+	 * 将抛出的异常格式化输出
+	 * 
+	 * @param e Exception
+	 * @return
+	 */
+	public static String exceptionToString(Exception e) {
+		ArrayList<String> list = new ArrayList<>();
+		list.add(e.toString());
+		for (StackTraceElement element : e.getStackTrace()) {
+			list.add(element.toString());
+		}
+		return String.join("\n\t\t", list);
+	}
+}
