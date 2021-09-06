beb6a924633c108d920ffded647d5b73879474ba
CuiiGen <1029374457@qq.com>
2021-09-06 14:26:54 +0800

    删除无用代码

--- src/main/MainFrame.java
+++ src/main/MainFrame.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -18,7 +17,6 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.plaf.basic.BasicMenuItemUI;
 import javax.swing.plaf.basic.BasicMenuUI;
-import javax.swing.table.AbstractTableModel;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -30,7 +28,6 @@ import dialogs.InfoDialog;
 import dialogs.LabelsDialog;
 import dialogs.MessageDialog;
 import dialogs.TransferDialog;
-import models.RecordStructure;
 import panels.AccountsPanel;
 import panels.LedgerPanel;
 import panels.QueryConditions;
@@ -159,8 +156,7 @@ public class MainFrame extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == mit[ITEM_ABOUT]) {
 			// 关于
-			MessageDialog.showMessage(this,
-					"我的账本Ledger V3.5.3，由iamroot开发使用\r\n" + "时间：2021年8月26日\r\n" + "邮箱：cuigen@buaa.edu.cn");
+			MessageDialog.showMessage(this, "我的账本Ledger V3.5.3，由iamroot开发\r\n时间：2021年8月26日\r\n邮箱：cuigen@buaa.edu.cn");
 		} else if (e.getSource() == mit[ITEM_LABEL]) {
 			// 标签管理
 			logger.info("打开标签管理对话框");
@@ -304,67 +300,4 @@ class DefaultMenuUI extends BasicMenuUI {
 		super.selectionBackground = bgColor;
 		super.selectionForeground = fgColor;
 	}
-}
-
-class RecordsModel extends AbstractTableModel {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2453177164571829047L;
-
-	private static String[] title = { "记账时间", "相关账户", "类型", "金额", "标签", "备注" };
-
-	private ArrayList<RecordStructure> array = new ArrayList<>();
-
-	public RecordsModel(ArrayList<RecordStructure> array) {
-		this.array = array;
-	}
-
-	@Override
-	public String getColumnName(int column) {
-		return title[column];
-	}
-
-	@Override
-	public int getRowCount() {
-		return array.size();
-	}
-
-	@Override
-	public int getColumnCount() {
-		return title.length;
-	}
-
-	@Override
-	public Object getValueAt(int rowIndex, int columnIndex) {
-		Object o = null;
-		switch (columnIndex) {
-		case 0:
-			o = array.get(rowIndex).getCreatetime();
-			break;
-		case 1:
-			o = array.get(rowIndex).getName();
-			break;
-		case 2:
-			o = array.get(rowIndex).getType() == -1 ? "支出" : "收入";
-			break;
-		case 3:
-			o = array.get(rowIndex).getAmount();
-			break;
-		case 4:
-			o = array.get(rowIndex).getLabel();
-			break;
-		case 5:
-			o = array.get(rowIndex).getRemark();
-			break;
-		}
-		return o;
-	}
-
-	@Override
-	public boolean isCellEditable(int rowIndex, int columnIndex) {
-		return false;
-	}
-
 }
